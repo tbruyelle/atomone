da8c4022519919e44d0257d37605a2cd6ab537cf
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2024-10-11 18:08:08 +0200

    test: ValidateGovernorMinSelfDelegation

--- x/gov/keeper/governor_test.go
+++ x/gov/keeper/governor_test.go
@@ -4,10 +4,13 @@ import (
 	"testing"
 	"time"
 
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	simtestutil "github.com/cosmos/cosmos-sdk/testutil/sims"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 
 	v1 "github.com/atomone-hub/atomone/x/gov/types/v1"
 )
@@ -79,5 +82,110 @@ func TestGovernor(t *testing.T) {
 				assert.Equal(gov2, *govs[i])
 			}
 		}
+	}
+}
+
+func TestValidateGovernorMinSelfDelegation(t *testing.T) {
+	var (
+		addrs = simtestutil.CreateRandomAccounts(2)
+		// TODO add multiple validator and delegations
+		valAddr   = simtestutil.ConvertAddrsToValAddrs(addrs[:1])[0]
+		validator = stakingtypes.Validator{
+			OperatorAddress: valAddr.String(),
+			Status:          stakingtypes.Bonded,
+			Tokens:          sdk.OneInt(),
+			DelegatorShares: sdk.OneDec(),
+		}
+		govAddr = convertAddrsToGovAddrs(addrs[1:])[0]
+	)
+	tests := []struct {
+		name           string
+		governor       v1.Governor
+		selfDelegation bool
+		valDelegations []stakingtypes.Delegation
+		expectedPanic  bool
+		expectedValid  bool
+	}{
+		{
+			name:           "inactive governor",
+			governor:       v1.Governor{GovernorAddress: govAddr.String(), Status: v1.Inactive},
+			selfDelegation: false,
+			valDelegations: nil,
+			expectedPanic:  false,
+			expectedValid:  false,
+		},
+		{
+			name:           "active governor w/o self delegation w/o validator delegation",
+			governor:       v1.Governor{GovernorAddress: govAddr.String(), Status: v1.Active},
+			selfDelegation: false,
+			valDelegations: nil,
+			expectedPanic:  true,
+			expectedValid:  false,
+		},
+		{
+			name:           "active governor w self delegation w/o validator delegation",
+			governor:       v1.Governor{GovernorAddress: govAddr.String(), Status: v1.Active},
+			selfDelegation: true,
+			valDelegations: nil,
+			expectedPanic:  false,
+			expectedValid:  false,
+		},
+		{
+			name:           "active governor w self delegation w not enough validator delegation",
+			governor:       v1.Governor{GovernorAddress: govAddr.String(), Status: v1.Active},
+			selfDelegation: true,
+			valDelegations: []stakingtypes.Delegation{
+				{
+					DelegatorAddress: govAddr.String(),
+					ValidatorAddress: valAddr.String(),
+					Shares:           sdk.OneDec(),
+				},
+			},
+			expectedPanic: false,
+			expectedValid: false,
+		},
+		{
+			name:           "active governor w self delegation w enough validator delegation",
+			governor:       v1.Governor{GovernorAddress: govAddr.String(), Status: v1.Active},
+			selfDelegation: true,
+			valDelegations: []stakingtypes.Delegation{
+				{
+					DelegatorAddress: govAddr.String(),
+					ValidatorAddress: valAddr.String(),
+					Shares:           v1.DefaultMinGovernorSelfDelegation.ToLegacyDec(),
+				},
+			},
+			expectedPanic: false,
+			expectedValid: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			govKeeper, mocks, _, ctx := setupGovKeeper(t, mockAccountKeeperExpectations)
+			// setup validator delegations expectation
+			mocks.stakingKeeper.EXPECT().GetValidator(ctx, valAddr).Return(validator, true).AnyTimes()
+			delAddr := sdk.AccAddress(govAddr)
+			mocks.stakingKeeper.EXPECT().IterateDelegations(ctx, delAddr, gomock.Any()).
+				DoAndReturn(
+					func(_ sdk.Context, _ sdk.AccAddress, f func(int64, stakingtypes.DelegationI) bool) {
+						for i, d := range tt.valDelegations {
+							if f(int64(i), d) {
+								return
+							}
+						}
+					},
+				).MaxTimes(2)
+			if tt.selfDelegation {
+				govKeeper.DelegateToGovernor(ctx, delAddr, govAddr)
+			}
+
+			if tt.expectedPanic {
+				assert.Panics(t, func() { govKeeper.ValidateGovernorMinSelfDelegation(ctx, tt.governor) })
+			} else {
+				valid := govKeeper.ValidateGovernorMinSelfDelegation(ctx, tt.governor)
+
+				assert.Equal(t, tt.expectedValid, valid, "return of ValidateGovernorMinSelfDelegation")
+			}
+		})
 	}
 }
