74aa2bda71e69fd505d879ac700c594716311153
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2025-01-04 22:48:29 +0100

    complete create multisig

--- x/multisig/keeper/keeper.go
+++ x/multisig/keeper/keeper.go
@@ -2,6 +2,8 @@ package keeper
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/binary"
 	"fmt"
 
 	"github.com/cometbft/cometbft/libs/log"
@@ -18,8 +20,9 @@ type Keeper struct {
 	cdc      codec.BinaryCodec
 	storeKey storetypes.StoreKey
 
-	schema    collections.Schema
-	multisigs collections.Map[[]byte, types.Multisig]
+	schema         collections.Schema
+	multisigs      collections.Map[[]byte, types.Multisig]
+	multisigNumber collections.Sequence
 
 	authority string
 }
@@ -42,7 +45,8 @@ func NewKeeper(
 			collections.BytesKey,
 			collcodec.CollValue[types.Multisig](cdc),
 		),
-		authority: authority,
+		multisigNumber: collections.NewSequence(sb, types.KeyMultisigNumbger, "multisig_number"),
+		authority:      authority,
 	}
 	schema, err := sb.Build()
 	if err != nil {
@@ -54,4 +58,28 @@ func NewKeeper(
 
 func (k Keeper) Logger(ctx sdk.Context) log.Logger {
 	return ctx.Logger().With("module", fmt.Sprintf("x/%s", types.ModuleName))
+}
+
+// NOTE copied from x/accounts
+// makeAddress creates an address for the given account.
+// It uses the creator address to ensure address squatting cannot happen, for example
+// assuming creator sends funds to a new account X nobody can front-run that address instantiation
+// unless the creator itself sends the tx.
+// AddressSeed can be used to create predictable addresses, security guarantees of the above are retained.
+// If address seed is not provided, the address is created using the creator and account number.
+func (k Keeper) makeAddress(creator []byte, accNum uint64, addressSeed []byte) ([]byte, error) {
+	// in case an address seed is provided, we use it to create the address.
+	var seed []byte
+	if len(addressSeed) > 0 {
+		seed = append(creator, addressSeed...)
+	} else {
+		// otherwise we use the creator and account number to create the address.
+		seed = append(creator, binary.BigEndian.AppendUint64(nil, accNum)...)
+	}
+
+	moduleAndSeed := append([]byte(types.ModuleName), seed...)
+
+	addr := sha256.Sum256(moduleAndSeed)
+
+	return addr[:], nil
 }
